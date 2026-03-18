988b34d3b3831a891d0566cdbc2dc4a183ac8c21
Matheus Ferreira <mathxtzferreira@gmail.com>
2026-03-18 20:22:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const imagens = [
         nome: "Início da Nossa História", 
         img: "IMG_6787_jpg.JPG", 
         album: albumHistoria,
-        musica: "O Amor Coloriu- Luan Santana - Luan Santana (youtube).mp3" // Nome da música do 1º álbum
+        musica: "Luan Santana - TE VIVO (Registro Histórico) - Luan Santana (youtube).mp3" // Nome da música do 1º álbum
     },
     { 
         nome: "Formando", 
